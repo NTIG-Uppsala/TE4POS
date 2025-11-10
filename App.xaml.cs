11bb667adfa095f46d63591ce7fc0e30d7e444c5
pysor2 <erikssonrazze@gmail.com>
2025-11-10 12:02:31 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TE4POS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
