16a050cb419ffba2748a5746a615134f2c06ca8b
pysor2 <erikssonrazze@gmail.com>
2025-11-13 15:19:57 +0100

    What I've made so far on new design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace TE4POS
 {
@@ -16,42 +10,17 @@ namespace TE4POS
     /// </summary>
     public partial class MainWindow : Window
     {
-        int kaffe = 0;
-        int total;
+        private Products products;
         public MainWindow()
         {
             InitializeComponent();
+            products = new Products();
+            DataContext = products;
         }
 
-        private void Add_Coffee_Click(object sender, RoutedEventArgs e)
+        private void addAmount(object sender, RoutedEventArgs e)
         {
-            kaffe++;
-            kaffenr.Clear();
-            kaffenr.AppendText(kaffe.ToString());
-            sum.Clear();
-            total = kaffe * 49;
-            sum.AppendText(total.ToString());
-        }
 
-        private void Remove_Coffee_Click(object sender, RoutedEventArgs e)
-        {
-            if (kaffe > 0)
-            {
-                kaffe--;
-                kaffenr.Clear();
-                kaffenr.AppendText(kaffe.ToString());
-            }
-            sum.Clear();
-            total = kaffe * 49;
-            sum.AppendText(total.ToString());
-        }
-
-        private void Reset_Click(object sender, RoutedEventArgs e)
-        {
-            kaffe = 0;
-            kaffenr.Clear();
-            kaffenr.AppendText(kaffe.ToString());
-            sum.Clear();
         }
     }
 }
