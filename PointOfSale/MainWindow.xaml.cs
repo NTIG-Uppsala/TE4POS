93783d568ae8e67868e25ab5a2a123a4b4bc25e1
LinusEW <elistewesterlinus@gmail.com>
2025-11-14 16:29:18 +0100

    Adds the funtionaility to reset the cart
    You can now reset the cart with either the red or the green button.
    Renamed some variables to make it easier to understand and added some comments.

--- PointOfSale/MainWindow.xaml.cs
+++ PointOfSale/MainWindow.xaml.cs
@@ -1,61 +1,101 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Xml.Linq;
 
 namespace TE4POS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // A list of all products available in the store (binds to the UI)
         public ObservableCollection<Product> AllProducts { get; set; }
-        public ObservableCollection<Product> ProductsInCart { get; set; }
+
+        // A list of all products added to the cart (also binds to the UI)
+        public ObservableCollection<CartItem> ShoppingCart { get; set; }
+
+        public int ShoppingCartTotalPrice
+        {
+            get
+            {
+                return ShoppingCart.Sum(item => item.Price * item.Amount);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Create some example products
             AllProducts = new ObservableCollection<Product>
             {
-                new Product { Name = "Apple", Price = 10, Amount = 1 },
-                new Product { Name = "Banana", Price = 5, Amount = 2 },
-                new Product { Name = "Orange", Price = 8, Amount = 1 }
+                new Product { Name = "Apple", Price = 10 },
+                new Product { Name = "Banana", Price = 5 },
+                new Product { Name = "Orange", Price = 8 }
             };
 
-            ProductsInCart = new ObservableCollection<Product>{};
+            // An empty cart
+            ShoppingCart = new ObservableCollection<CartItem>{};
 
+            // Makes bindings look for properties inside this class
             DataContext = this;
+
         }
 
         private void AddAmount_Click(object sender, RoutedEventArgs e)
         {
+            // Check that the sender is a UI element and that it has a Product as DataContext
             if (sender is FrameworkElement fe && fe.DataContext is Product product)
             {
-                new ProductInCart 
+
+                var existingItem = ShoppingCart.FirstOrDefault(x => x.Name == product.Name);
+
+                if (existingItem != null)
                 {
-                    Name = product.Name,
-                    Price = product.Price,
-                    Amount = product.Amount
-                };
-                if (!ProductsInCart.Contains(product))
-                    ProductsInCart.Add(product);
+                    // If the product is already in the cart, increase the amount
+                    existingItem.Amount++;
+                    ShoppingCartTotal.Text += existingItem.Price;
+
+                }
                 else
                 {
-                    product.Amount++;
+                    // If the product is NOT in the cart, add it with amount 1
+                    ShoppingCart.Add(new CartItem
+                    {
+                        Name = product.Name,
+                        Price = product.Price,
+                        Amount = 1
+                    });
                 }
+                // Update the total price (displayed)
+                ShoppingCartTotal.Text = ShoppingCartTotalPrice.ToString();
             }
+        }
+
+        private void ResetCart_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var cartItem in ShoppingCart)
+            {
+                // Reset the amount of each product in the cart to 1
+                cartItem.Amount = 1;
+            }
+
+            ShoppingCart.Clear();
+            ShoppingCartTotal.Text = ShoppingCartTotalPrice.ToString();
         }
     }
 
-    public class Product : INotifyPropertyChanged
+    public class Product
     {
-         public int _amount;
-         public string Name { get; set; }
-         public int Price { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
 
+    }
+    public class CartItem : Product, INotifyPropertyChanged
+    {
+        // Backing field for Amount (needed for OnPropertyChanged)
+        private int _amount;
+
+        // Amount property notifies the UI when the value changes
         public int Amount
         {
             get => _amount;
@@ -65,15 +105,13 @@ namespace TE4POS
                 OnPropertyChanged();
             }
         }
-
+        // Standard INotifyPropertyChanged implementation
         public event PropertyChangedEventHandler PropertyChanged;
-            public void OnPropertyChanged([CallerMemberName] string propertyName = null)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-    }
-    public class ProductInCart : Product
-    {
 
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Notify WPF that a property changed so the UI updates
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
